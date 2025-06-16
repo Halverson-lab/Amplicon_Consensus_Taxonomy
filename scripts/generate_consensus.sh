b2e3d4e7b792a2ba780a033fd82fe8b1a06148dc
Ashley A. Paulsen <43075823+ashleyp1@users.noreply.github.com>
2025-06-16 15:52:47 -0500

    Update generate_consensus.sh

--- scripts/generate_consensus.sh
+++ scripts/generate_consensus.sh
@@ -11,10 +11,10 @@ source config.txt
 [[ -z "$LIBRARY" ]] && { echo "LIBRARY is empty" ; exit 1; }
 [[ -z "$BARCODES" ]] && { echo "BARCODES is empty" ; exit 1; }
 
-[[ -z "$(ls -A $WORK_DIR/6_emu/read_assignments/)" ]] && { echo "${WORK_DIR}/6_emu/read_assignments/ is empty"; exit 1; }
-[[ -z "$(ls -A $WORK_DIR/7_sintax/)" ]] && { echo "${WORK_DIR}/7_sintax/ is empty"; exit 1; }
-[[ ! -e $WORK_DIR/5_laca/quant/seqID_to_otu.tsv ]] && { echo "5_laca/quant/seqID_to_otu.tsv does not exist"; exit 1; }
-[[ ! -e $WORK_DIR/5_laca/sintax_OTUs.tsv ]] && { echo "5_laca/sintax_OTUs.tsv does not exist"; exit 1; }
+[[ -z "$(ls -A $EMU_OUT/read_assignments/)" ]] && { echo "${EMU_OUT}/read_assignments/ is empty"; exit 1; }
+[[ -z "$(ls -A $SINTAX_OUT)" ]] && { echo "${SINTAX_OUT} is empty"; exit 1; }
+[[ ! -e $LACA_OUT/quant/seqID_to_otu.tsv ]] && { echo "${LACA_OUT}/quant/seqID_to_otu.tsv does not exist"; exit 1; }
+[[ ! -e $LACA_OUT/sintax_OTUs.tsv ]] && { echo "${LACA_OUT}/OTU_sintax.tsv does not exist"; exit 1; }
 
 
 run_flag=false
