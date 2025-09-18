0394f2a8bd04e4c0a1cf0cbe6d9df2c5a25cbb7e
Ashley A. Paulsen <43075823+ashleyp1@users.noreply.github.com>
2025-09-18 12:18:47 -0500

    Update database_builder.sh

--- scripts/database_builder.sh
+++ scripts/database_builder.sh
@@ -115,187 +115,189 @@ cd $DATABASE_DIR
 rm database_builder.log
 exec 1> database_builder.log
 
-if [[ $default_flag  == "true" ]]; then
-    #build with latest NCBI RefSeq and the rrnDB
-
-    #download database
-    [[ ! -e blast_16S_DB ]] && { mkdir blast_16S_DB ; } 
-    cd blast_16S_DB 
-    update_blastdb.pl --decompress 16S_ribosomal_RNA
-    #retrieve fasta
-    blastdbcmd -entry all -db 16S_ribosomal_RNA -out ../ncbi_16S.fasta
-    cd $DATABASE_DIR
-
-    #if the fasta file was not provided then download a new one with the provided URL
-    if [[ ! -e $DATABASE_DIR/rrnDB.fasta ]]; then
-        wget $RRNDB
-        unzip rrnDB*.zip && rm -r rrnDB*.zip
-        mv rrnDB*.fasta ./rrnDB.fasta 
-    fi
-
-    ## re-format rrnDB formatted files
-    # For later application we need fasta headers starting with useful sequence IDs
-    # get fasta headers
-    seqkit seq -n rrnDB.fasta > rrnDB_header_list.txt
-
-    # get the RefSeq numbers from each header and generate new headers
-    cat rrnDB_header_list.txt \
-        | csvtk add-header -t -n fasta_header \
-        | csvtk mutate -t -f 1 -p "(?:[^\|]*\|){2}(.*?)\|" -n refSeq \
-        | csvtk mutate2 -t -n newHeader -e '$refSeq + " " + $fasta_header'> rrnDB_header_refseq_list.txt 
-
-    # File of rrnDB header to new rrnDB header with RefSeq ID as the sequence ID
-    cat rrnDB_header_refseq_list.txt | csvtk cut -t -f 1,3 | csvtk del-header -t > old2newheader.tsv
-    # replace the headers
-    seqkit replace -p '(.+)' -r '{kv}' -k old2newheader.tsv rrnDB.fasta > rrnDB_renamed.fasta
-
-    # Use refSeq numbers to get taxid
-    cat rrnDB_header_refseq_list.txt | csvtk cut -t -f 2 | csvtk del-header -t | epost -db nuccore | esummary | xtract -pattern DocumentSummary -element AccessionVersion,TaxId > rrnDB_seq2taxid.txt
-
-    # Filter out any of the fasta seq that were removed
-    csvtk cut -t -f 1 rrnDB_seq2taxid.txt > seq2keep.txt
-    seqkit grep -f seq2keep.txt rrnDB_renamed.fasta -o ACT_rrnDB.fasta
-
-    #ACT_rrnDB.fasta and rrnDB_seq2taxid.txt are the only ones we need to keep
-    rm rrnDB_header_list.txt rrnDB_header_refseq_list.txt old2newheader.tsv rrnDB_renamed.fasta seq2keep.txt
-
-    ### re-format NCBI formatted files
-    #get fasta headers
-    seqkit seq -n ncbi_16S.fasta > ncbi_16S_header.txt
-
-    #get the RefSeq numbers from each header
-    cat ncbi_16S_header.txt | csvtk add-header -t -n fasta_header | csvtk mutate -t -f 1 -p "([^\s]+)" -n refSeq > ncbi_16S_header_refseq.txt
-
-    #Use refSeq numbers to get taxid
-    cat ncbi_16S_header_refseq.txt | csvtk cut -t -f 2 | csvtk del-header -t | epost -db nuccore | esummary | xtract -pattern DocumentSummary -element AccessionVersion,TaxId > refseq2taxid.txt
-
-    csvtk add-header -t -n refSeq,taxid refseq2taxid.txt > refseq2taxid2.txt
-
-    # Any seqs where the RefSeq record was suppressed will be removed from the list (this shouldn't change anything as long as your fasta file is current)
-    csvtk join -t -f refSeq ncbi_16S_header_refseq.txt refseq2taxid2.txt | csvtk cut -t -f 2,3 |csvtk del-header -t > ncbi_seq2taxid.txt
-
-    # ncbi_16S.fasta and ncbi_seq2taxid.txt are the ones to keep
-    rm ncbi_16S_header.txt ncbi_16S_header_refseq.txt refseq2taxid.txt refseq2taxid2.txt
-
-    # combine them into one 
-    cat rrnDB_seq2taxid.txt ncbi_seq2taxid.txt > seq2taxid.txt
-    cat ACT_rrnDB.fasta ncbi_16S.fasta > sequences.fasta
-
-
-    # Build the taxonomy table
-    # getting the NCBI taxonomy
-    wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
-    mkdir taxdump
-    tar -xzf taxdump.tar.gz -C taxdump && rm taxdump.tar.gz
-    # Tell taxonkit where to find taxdump
-    export TAXONKIT_DB=$DATABASE_DIR/taxdump
-
-    # Build taxonomy.tsv
-    csvtk cut -t -f 2 seq2taxid.txt \
-    | taxonkit lineage \
-    | taxonkit reformat -t -f "{d}\t{p}\t{c}\t{o}\t{f}\t{g}\t{s}" \
-    | csvtk cut -t -f -2 \
-    | csvtk add-header -t -n taxid,domain,phylum,class,order,family,genus,species,t_domain,t_phylum,t_class,t_order,t_family,t_genus,t_species -o taxonomy.tsv
-fi
-
-if [[ $sintax_flag == "true" ]]; then
-
-    seqkit seq -n $USER_SEQ | csvtk add-header -t -n seqid -o read_taxon_list.tsv
+if [[ $default_flag  == "true" || $sintax_flag == "true" || $ncbi_flag == "true" || $add_flag == "true" ]]; then
+    if [[ $default_flag  == "true" ]]; then
+        #build with latest NCBI RefSeq and the rrnDB
     
-    cat read_taxon_list.tsv \
-        | csvtk sep -t -f 1 -s ";" -n seq_id,taxon,tmp \
-        | csvtk cut -t -f 2,3 \
-        | csvtk mutate -t -f taxon -n domain -p "d:(.*)" --na \
-        | csvtk mutate -t -f taxon -n phylum -p "p:(.*)" --na \
-        | csvtk mutate -t -f taxon -n class -p "c:(.*)" --na \
-        | csvtk mutate -t -f taxon -n order -p "o:(.*)" --na \
-        | csvtk mutate -t -f taxon -n family -p "f:(.*)" --na \
-        | csvtk mutate -t -f taxon -n genus -p "g:(.*)" --na \
-        | csvtk mutate -t -f taxon -n species -p "s:(.*)" --na \
-        | csvtk replace -t -f domain -p ",.*" -r "" \
-        | csvtk replace -t -f phylum -p ",.*" -r "" \
-        | csvtk replace -t -f class -p ",.*" -r "" \
-        | csvtk replace -t -f order -p ",.*" -r "" \
-        | csvtk replace -t -f family -p ",.*" -r "" \
-        | csvtk replace -t -f genus -p ",.*" -r "" \
-        | csvtk replace -t -f species -p ",.*" -r "" \
-        | csvtk cut -t -f -2 > taxonomy_for_taxdump.tsv
-        
-    taxonkit create-taxdump -A 1 taxonomy_for_taxdump.tsv --out-dir taxdump
+        #download database
+        [[ ! -e blast_16S_DB ]] && { mkdir blast_16S_DB ; } 
+        cd blast_16S_DB 
+        update_blastdb.pl --decompress 16S_ribosomal_RNA
+        #retrieve fasta
+        blastdbcmd -entry all -db 16S_ribosomal_RNA -out ../ncbi_16S.fasta
+        cd $DATABASE_DIR
     
-    rm read_taxon_list.tsv taxonomy_for_taxdump.tsv
-    
-    #tell taxonkit where to find the new taxdump files
-    export TAXONKIT_DB=taxdump/
-    
-    #cp the taxid.map to make it easier to find
-    cp taxdump/taxid.map ./seq2tax_map.txt
-    
-    csvtk cut -t -f 2 seq2tax_map.txt \
-        | taxonkit lineage \
-        | taxonkit reformat -t -f "{k}\t{p}\t{c}\t{o}\t{f}\t{g}\t{s}" \
-        | csvtk cut -t -f -2 \
-        | csvtk add-header -t -n taxid,domain,phylum,class,order,family,genus,species,t_domain,t_phylum,t_class,t_order,t_family,t_genus,t_species -o taxonomy.tsv
-
-fi
-
-if [[ $ncbi_flag == "true" ]]; then
-    #retieve based on list of accessions
-    #/db_xref="RFAM:RF00177"
-    ## takes in an accession list and spits out fasta files with the header locus_tag, record_name, and taxon:taxid  
-    for ACC in $(cat $ACC_LIST); do 
-        #download the genbank file
-        esearch -db nuccore -query "${ACC}" | efetch -format gbwithparts > tmp.gbk
-
-        if [ ! -s "tmp.gbk" ]; then 
-            echo "Accession ${ACC} is invalid, please double check the input" >&2
-        
-        else
-            #script to extract 16S rRNA sequences from the genbank file as fastas
-            extract_16S_from_genbank_to_fasta.py tmp.gbk "${ACC%.*}".fasta
-
-            #Append the fastas onto the fasta file
-            cat "${ACC%.*}".fasta >> sequences.fasta
-
-            #add seqid to taxid
-            #get fasta headers
-            seqkit seq -n "${ACC%.*}".fasta \
-            | csvtk add-header -t -n fasta_header \
-            | csvtk mutate -t -f 1 -p "taxon:(\d+)" -n taxid \
-            | csvtk mutate -t -f 1 -p "([^\s]+)" -n ID \
-            | csvtk cut -t -f 3,2 \
-            | csvtk del-header -t > "${ACC%.*}"_seq2taxid.txt
-            cat "${ACC%.*}"_seq2taxid.txt >> seq2taxid.txt
-            rm "${ACC%.*}"*
+        #if the fasta file was not provided then download a new one with the provided URL
+        if [[ ! -e $DATABASE_DIR/rrnDB.fasta ]]; then
+            wget $RRNDB
+            unzip rrnDB*.zip && rm -r rrnDB*.zip
+            mv rrnDB*.fasta ./rrnDB.fasta 
         fi
-
-    done
-
-    #retrieve lineage info
-    export TAXONKIT_DB=$DATABASE_DIR/taxdump
-    csvtk cut -t -f 2 seq2taxid.txt \
+    
+        ## re-format rrnDB formatted files
+        # For later application we need fasta headers starting with useful sequence IDs
+        # get fasta headers
+        seqkit seq -n rrnDB.fasta > rrnDB_header_list.txt
+    
+        # get the RefSeq numbers from each header and generate new headers
+        cat rrnDB_header_list.txt \
+            | csvtk add-header -t -n fasta_header \
+            | csvtk mutate -t -f 1 -p "(?:[^\|]*\|){2}(.*?)\|" -n refSeq \
+            | csvtk mutate2 -t -n newHeader -e '$refSeq + " " + $fasta_header'> rrnDB_header_refseq_list.txt 
+    
+        # File of rrnDB header to new rrnDB header with RefSeq ID as the sequence ID
+        cat rrnDB_header_refseq_list.txt | csvtk cut -t -f 1,3 | csvtk del-header -t > old2newheader.tsv
+        # replace the headers
+        seqkit replace -p '(.+)' -r '{kv}' -k old2newheader.tsv rrnDB.fasta > rrnDB_renamed.fasta
+    
+        # Use refSeq numbers to get taxid
+        cat rrnDB_header_refseq_list.txt | csvtk cut -t -f 2 | csvtk del-header -t | epost -db nuccore | esummary | xtract -pattern DocumentSummary -element AccessionVersion,TaxId > rrnDB_seq2taxid.txt
+    
+        # Filter out any of the fasta seq that were removed
+        csvtk cut -t -f 1 rrnDB_seq2taxid.txt > seq2keep.txt
+        seqkit grep -f seq2keep.txt rrnDB_renamed.fasta -o ACT_rrnDB.fasta
+    
+        #ACT_rrnDB.fasta and rrnDB_seq2taxid.txt are the only ones we need to keep
+        rm rrnDB_header_list.txt rrnDB_header_refseq_list.txt old2newheader.tsv rrnDB_renamed.fasta seq2keep.txt
+    
+        ### re-format NCBI formatted files
+        #get fasta headers
+        seqkit seq -n ncbi_16S.fasta > ncbi_16S_header.txt
+    
+        #get the RefSeq numbers from each header
+        cat ncbi_16S_header.txt | csvtk add-header -t -n fasta_header | csvtk mutate -t -f 1 -p "([^\s]+)" -n refSeq > ncbi_16S_header_refseq.txt
+    
+        #Use refSeq numbers to get taxid
+        cat ncbi_16S_header_refseq.txt | csvtk cut -t -f 2 | csvtk del-header -t | epost -db nuccore | esummary | xtract -pattern DocumentSummary -element AccessionVersion,TaxId > refseq2taxid.txt
+    
+        csvtk add-header -t -n refSeq,taxid refseq2taxid.txt > refseq2taxid2.txt
+    
+        # Any seqs where the RefSeq record was suppressed will be removed from the list (this shouldn't change anything as long as your fasta file is current)
+        csvtk join -t -f refSeq ncbi_16S_header_refseq.txt refseq2taxid2.txt | csvtk cut -t -f 2,3 |csvtk del-header -t > ncbi_seq2taxid.txt
+    
+        # ncbi_16S.fasta and ncbi_seq2taxid.txt are the ones to keep
+        rm ncbi_16S_header.txt ncbi_16S_header_refseq.txt refseq2taxid.txt refseq2taxid2.txt
+    
+        # combine them into one 
+        cat rrnDB_seq2taxid.txt ncbi_seq2taxid.txt > seq2taxid.txt
+        cat ACT_rrnDB.fasta ncbi_16S.fasta > sequences.fasta
+    
+    
+        # Build the taxonomy table
+        # getting the NCBI taxonomy
+        wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
+        mkdir taxdump
+        tar -xzf taxdump.tar.gz -C taxdump && rm taxdump.tar.gz
+        # Tell taxonkit where to find taxdump
+        export TAXONKIT_DB=$DATABASE_DIR/taxdump
+    
+        # Build taxonomy.tsv
+        csvtk cut -t -f 2 seq2taxid.txt \
         | taxonkit lineage \
         | taxonkit reformat -t -f "{d}\t{p}\t{c}\t{o}\t{f}\t{g}\t{s}" \
         | csvtk cut -t -f -2 \
         | csvtk add-header -t -n taxid,domain,phylum,class,order,family,genus,species,t_domain,t_phylum,t_class,t_order,t_family,t_genus,t_species -o taxonomy.tsv
+    fi
+    
+    if [[ $sintax_flag == "true" ]]; then
+    
+        seqkit seq -n $USER_SEQ | csvtk add-header -t -n seqid -o read_taxon_list.tsv
+        
+        cat read_taxon_list.tsv \
+            | csvtk sep -t -f 1 -s ";" -n seq_id,taxon,tmp \
+            | csvtk cut -t -f 2,3 \
+            | csvtk mutate -t -f taxon -n domain -p "d:(.*)" --na \
+            | csvtk mutate -t -f taxon -n phylum -p "p:(.*)" --na \
+            | csvtk mutate -t -f taxon -n class -p "c:(.*)" --na \
+            | csvtk mutate -t -f taxon -n order -p "o:(.*)" --na \
+            | csvtk mutate -t -f taxon -n family -p "f:(.*)" --na \
+            | csvtk mutate -t -f taxon -n genus -p "g:(.*)" --na \
+            | csvtk mutate -t -f taxon -n species -p "s:(.*)" --na \
+            | csvtk replace -t -f domain -p ",.*" -r "" \
+            | csvtk replace -t -f phylum -p ",.*" -r "" \
+            | csvtk replace -t -f class -p ",.*" -r "" \
+            | csvtk replace -t -f order -p ",.*" -r "" \
+            | csvtk replace -t -f family -p ",.*" -r "" \
+            | csvtk replace -t -f genus -p ",.*" -r "" \
+            | csvtk replace -t -f species -p ",.*" -r "" \
+            | csvtk cut -t -f -2 > taxonomy_for_taxdump.tsv
+            
+        taxonkit create-taxdump -A 1 taxonomy_for_taxdump.tsv --out-dir taxdump
+        
+        rm read_taxon_list.tsv taxonomy_for_taxdump.tsv
+        
+        #tell taxonkit where to find the new taxdump files
+        export TAXONKIT_DB=taxdump/
+        
+        #cp the taxid.map to make it easier to find
+        cp taxdump/taxid.map ./seq2tax_map.txt
+        
+        csvtk cut -t -f 2 seq2tax_map.txt \
+            | taxonkit lineage \
+            | taxonkit reformat -t -f "{k}\t{p}\t{c}\t{o}\t{f}\t{g}\t{s}" \
+            | csvtk cut -t -f -2 \
+            | csvtk add-header -t -n taxid,domain,phylum,class,order,family,genus,species,t_domain,t_phylum,t_class,t_order,t_family,t_genus,t_species -o taxonomy.tsv
+    
+    fi
+    
+    if [[ $ncbi_flag == "true" ]]; then
+        #retieve based on list of accessions
+        #/db_xref="RFAM:RF00177"
+        ## takes in an accession list and spits out fasta files with the header locus_tag, record_name, and taxon:taxid  
+        for ACC in $(cat $ACC_LIST); do 
+            #download the genbank file
+            esearch -db nuccore -query "${ACC}" | efetch -format gbwithparts > tmp.gbk
+    
+            if [ ! -s "tmp.gbk" ]; then 
+                echo "Accession ${ACC} is invalid, please double check the input" >&2
+            
+            else
+                #script to extract 16S rRNA sequences from the genbank file as fastas
+                extract_16S_from_genbank_to_fasta.py tmp.gbk "${ACC%.*}".fasta
+    
+                #Append the fastas onto the fasta file
+                cat "${ACC%.*}".fasta >> sequences.fasta
+    
+                #add seqid to taxid
+                #get fasta headers
+                seqkit seq -n "${ACC%.*}".fasta \
+                | csvtk add-header -t -n fasta_header \
+                | csvtk mutate -t -f 1 -p "taxon:(\d+)" -n taxid \
+                | csvtk mutate -t -f 1 -p "([^\s]+)" -n ID \
+                | csvtk cut -t -f 3,2 \
+                | csvtk del-header -t > "${ACC%.*}"_seq2taxid.txt
+                cat "${ACC%.*}"_seq2taxid.txt >> seq2taxid.txt
+                rm "${ACC%.*}"*
+            fi
+    
+        done
+    
+        #retrieve lineage info
+        export TAXONKIT_DB=$DATABASE_DIR/taxdump
+        csvtk cut -t -f 2 seq2taxid.txt \
+            | taxonkit lineage \
+            | taxonkit reformat -t -f "{d}\t{p}\t{c}\t{o}\t{f}\t{g}\t{s}" \
+            | csvtk cut -t -f -2 \
+            | csvtk add-header -t -n taxid,domain,phylum,class,order,family,genus,species,t_domain,t_phylum,t_class,t_order,t_family,t_genus,t_species -o taxonomy.tsv
+    fi
+    
+    if [[ $add_flag == "true" ]]; then
+        # user provided
+        cat $USER_SEQ >> sequences.fasta
+        cat $USER_SEQ2TAX >> seq2taxid.txt
+        if [[ -z "$USER_TAX" ]]; then
+            cat $USER_TAX >> taxonomy.tsv
+        fi 
+    fi
+    
+    #remove special characters from the taxonomy
+    cat taxonomy.tsv | tr -d ';#&%' | tr -d "\'" > safe_taxonomy.tsv && mv ./safe_taxonomy.tsv ./taxonomy.tsv
+    
+    # Build EMU formatted database
+    emu build-database ACT_DB --sequences sequences.fasta --seq2tax seq2taxid.txt --taxonomy-list taxonomy.tsv
+    mv ACT_DB/* ./ 
+    rm -r ACT_DB
 fi
-
-if [[ $add_flag == "true" ]]; then
-    # user provided
-    cat $USER_SEQ >> sequences.fasta
-    cat $USER_SEQ2TAX >> seq2taxid.txt
-    if [[ -z "$USER_TAX" ]]; then
-        cat $USER_TAX >> taxonomy.tsv
-    fi 
-fi
-
-#remove special characters from the taxonomy
-cat taxonomy.tsv | tr -d ';#&%' | tr -d "\'" > safe_taxonomy.tsv && mv ./safe_taxonomy.tsv ./taxonomy.tsv
-
-# Build EMU formatted database
-emu build-database ACT_DB --sequences sequences.fasta --seq2tax seq2taxid.txt --taxonomy-list taxonomy.tsv
-mv ACT_DB/* ./ 
-rm -r ACT_DB
 
 # Directory should now contain species_taxid.fasta and taxonomy.tsv, whether just generated or user provided
 [[ -z species_taxid.fasta ]] && { echo "species_taxid.fasta does not exist, please provide database files or run database_builder.sh -d to build a default database" >&2; exit 1; }
